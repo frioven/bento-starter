26ac9272cba99804cc64bea4c74db75409a71765
frioven <frioven.chile@gmail.com>
2019-09-10 01:01:29 -0400

    inicial

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,14 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: "AIzaSyA_gMJlkh2sJcWiWCLR8rejv0duCIrPZ2g",
+  authDomain: "sitiowebfrioven.firebaseapp.com",
+  databaseURL: "https://sitiowebfrioven.firebaseio.com",
+  projectId: "sitiowebfrioven",
+  storageBucket: "sitiowebfrioven.appspot.com",
+  messagingSenderId: "69312608195",
+  appId: "1:69312608195:web:7f5dfa99512cae45622f76"
+
 }
 
 firebase.initializeApp(config)
